194cc20e787ac2b53ffb24346c4b2269af902e41
foreinsneun9 <winterreeeeeeee@gmail.com>
2026-03-09 11:23:32 +0000

    day6_etherpiggybank

--- foreinsneun_9/day6_etherpiggybank.sol
+++ foreinsneun_9/day6_etherpiggybank.sol
@@ -5,39 +5,41 @@ contract EtherPiggyBank {
     address[] members;
     mapping(address => bool) public registeredMembers;
     mapping(address => uint256) balance;
-    constructor(){
+    constructor() {
         bankManager = msg.sender;
         members.push(msg.sender);
+        registeredMembers[msg.sender] = true;
     }
-    modifier onlyBankManager(){
-        require(msg.sender == bankManager, "Only bank manager can perform this action");
+    modifier onlyBankManager() {
+        require(msg.sender == bankManager,"Only bank Manager can perform this action");
         _;
     }
-    modifier onlyRegisteredMember() {
-        require(registeredMembers[msg.sender], "Member not registered");
+    modifier onlyRegisteredMembers() {
+        require(registeredMembers[msg.sender],"Member not registered");
         _;
     }
-    function addMembers(address _member) public onlyBankManager{
-        require(_member != address(0), "Invalid address");
-        require(_member != msg.sender, "Bank Manager is already a member");
-        require(!registeredMembers[_member], "Member already registered");
+    function addMembers(address _member) public onlyBankManager {
+        require(_member!= address(0),"Invalid address");
+        require(_member != msg.sender,"Bank manager is already a member");
+        require(!registeredMembers[_member],"Member already registered");
         registeredMembers[_member] = true;
         members.push(_member);
     }
-    function getMembers() public view returns(address[] memory){
+    function getMembers() public view returns(address[] memory) {
         return members;
-    }    
-    function depositAmountEther() public payable onlyRegisteredMember{  
-        require(msg.value > 0, "Invalid amount");
-        balance[msg.sender] = balance[msg.sender] + msg.value;
     }
-    function withdrawAmount(uint256 _amount) public onlyRegisteredMember{
-        require(_amount > 0, "Invalid amount");
-        require(balance[msg.sender] >= _amount, "Insufficient balance");
-        balance[msg.sender] = balance[msg.sender] - _amount;
+    function depositAmountEther() public payable onlyRegisteredMembers {
+        require(msg.value > 0,"Invalid amount");
+        balance[msg.sender] += msg.value;
     }
-    function getBalance(address _member) public view returns (uint256){
-        require(_member != address(0), "Invalid address");
+    function withdrawAmount(uint256 _amount) public  onlyRegisteredMembers {
+        require(_amount > 0,"Invalid amount");
+        require(balance[msg.sender] >= _amount,"Insufficient balance");
+        balance[msg.sender] -= _amount;
+        payable(msg.sender) .transfer(_amount);
+    }
+    function getBalance(address _member) public  view returns (uint256) {
+        require(_member != address(0),"Invalid address");
         return balance[_member];
-    } 
+    }
 }
